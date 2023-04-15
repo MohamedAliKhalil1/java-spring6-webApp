7f8abc78b6b35c4056b631fa2c4a05b0bc4110b7
Mohamed Ali <mohamedalikahlil5@gmail.com>
2023-04-15 16:44:32 +0200

    added author spring data repository

--- src/main/java/ali/mohamed/spring6webapp/domain/repositories/AuthorRepository.java
+++ src/main/java/ali/mohamed/spring6webapp/domain/repositories/AuthorRepository.java
@@ -1,2 +1,7 @@
-package ali.mohamed.spring6webapp.domain.repositories;public interface AuthorRepository {
+package ali.mohamed.spring6webapp.domain.repositories;
+
+import ali.mohamed.spring6webapp.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
