328869f3c35e26eb5e0a86b6caf75fdee7c8937e
Mohamed Ali <mohamedalikahlil5@gmail.com>
2023-04-15 17:23:08 +0200

    added JPA repositories types

--- src/main/java/ali/mohamed/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/ali/mohamed/spring6webapp/bootstrap/BootstrapData.java
@@ -1,10 +1,21 @@
 package ali.mohamed.spring6webapp.bootstrap;
 
+import ali.mohamed.spring6webapp.repositories.AuthorRepository;
+import ali.mohamed.spring6webapp.repositories.BookRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BootstrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
     @Override
     public void run(String... args) throws Exception {
 
