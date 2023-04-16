7d62166f107bef93e70a5e36fed21db1a7b84001
MohamedAliKhalil1 <60011223+MohamedAliKhalil1@users.noreply.github.com>
2023-04-16 12:01:56 +0200

    Revert "publisher-entity"

--- src/main/java/ali/mohamed/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/ali/mohamed/spring6webapp/bootstrap/BootstrapData.java
@@ -2,10 +2,8 @@ package ali.mohamed.spring6webapp.bootstrap;
 
 import ali.mohamed.spring6webapp.domain.Author;
 import ali.mohamed.spring6webapp.domain.Book;
-import ali.mohamed.spring6webapp.domain.Publisher;
 import ali.mohamed.spring6webapp.repositories.AuthorRepository;
 import ali.mohamed.spring6webapp.repositories.BookRepository;
-import ali.mohamed.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -14,13 +12,10 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
-    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository,
-                         PublisherRepository publisherRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
-        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -56,12 +51,5 @@ public class BootstrapData implements CommandLineRunner {
         System.out.println("In Bootstrap");
         System.out.println("Author Count: " + authorRepository.count());
         System.out.println("Books Count: " + bookRepository.count());
-
-        Publisher publisher1 = new Publisher();
-        publisher1.setPublisherName("publisher1");
-        publisher1.setAddress("123 abc St.");
-        publisherRepository.save(publisher1);
-
-        System.out.println("Publisher Count: " + publisherRepository.count());
     }
 }
--- src/main/java/ali/mohamed/spring6webapp/repositories/PublisherRepository.java
+++
@@ -1,7 +0,0 @@
-package ali.mohamed.spring6webapp.repositories;
-
-import ali.mohamed.spring6webapp.domain.Publisher;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PublisherRepository extends CrudRepository<Publisher, Long> {
-}
